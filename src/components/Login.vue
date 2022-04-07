dccad63b5c6c4c924ead1818fd9adf8eb620a808
LINLvHua <the_hongyi@163.com>
2022-04-07 23:53:00 +0800

    forward:登录界面实现全段撰写

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <!-- v-model 实现双向数据绑定 -->
-    用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
-    <br><br>
-    密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
-    <br><br>
-    <button v-on:click="login">登录</button>
-  </div>
+  <body id="poster">
+    <el-form class="login-container" label-position="left"
+             label-width="0px">
+
+      <h3 class="login_title">系统登录</h3>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.username"
+                  auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" v-model="loginForm.password"
+                  auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item style="width: 100%">
+        <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+<!--    ————————————————-->
+<!--    版权声明：本文为CSDN博主「Evan-Nightly」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。-->
+<!--    原文链接：https://blog.csdn.net/Neuf_Soleil/article/details/89298717-->
+  </body>
 </template>
 
 <script>
@@ -40,3 +53,32 @@ export default {
   }
 }
 </script>
+<style>
+.login-container {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 90px auto;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+
+.login_title {
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: #505458;
+}
+#poster {
+  background:url("../assets/img.png") no-repeat;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: fixed;
+}
+body{
+  margin: 0px;
+}
+</style>
